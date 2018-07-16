bcf077ac23010ef3f717b2b5ec7c052f4e551d67
Danuta Przydrozna <danprzy@gmail.com>
2018-07-17 00:14:25 +0200

    Change style.css

--- App.js
+++ App.js
@@ -1,38 +1,10 @@
-/* OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
-} 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
-
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
-*/
-
 // ZMIENNE DO KOMUNIKACJI Z SERWEREM
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
     'X-Client-Id': '3193',
     'X-Auth-Token': '6056ef129460ec2f7a7d18767ffd1d56'
 };
-
+//dodania nagłówków bez konieczności umieszczania ich w każdym zapytaniu osobno
 $.ajaxSetup({
     headers: myHeaders
 });
@@ -49,7 +21,7 @@ $.ajax({
 function setupColumns(columns) {
     columns.forEach(function(column) {
         var col = new Column(column.id, column.name);
-        board.createColumn(col);
+        board.createColumn(col); //createColumn() w pliku Column.js).
         setupCards(col, column.cards);
     });
 }
@@ -57,6 +29,6 @@ function setupColumns(columns) {
 function setupCards(col, cards) {
     cards.forEach(function(card) {
         var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-        col.createCard(cardObj); //createCard() w pliku Column.js).
+        col.createCard(cardObj); //createCard() w pliku Card.js).
     });
 }
---
+++ README.md
@@ -0,0 +1,15 @@
+# Kanban board
+> This is a simple version of Kanban board.
+
+## Live Demo
+
+
+## Technologies
+* HTML5
+* CSS3
+* JavaScript
+* jQuery
+* jQueryUI
+
+
+
